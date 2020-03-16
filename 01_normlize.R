677613d5023f211a2d89c10830f01aa3c0a6d3c4
Bioinfoplanet <Bioplanet520@outlook.com>
2020-03-16 11:16:24 +0800

    first upload

--- 01_normlize.R
+++ 01_normlize.R
@@ -18,8 +18,19 @@ exp2 = log2(exp2+1)
 table(rownames(exp1) %in% rownames(exp2))
 length(intersect(rownames(exp1),rownames(exp2)))
 exp1 <- exp1[intersect(rownames(exp1),rownames(exp2)),]
-
 exp2 <- exp2[intersect(rownames(exp1),rownames(exp2)),]
+
+#exp = log2(exp+1)
+#(2)提取临床信息
+pd1 <- pData(eSet1[[1]])
+pd2 <- pData(eSet2[[1]])
+if(!identical(rownames(pd1),colnames(exp1))) exp1 = exp1[,match(rownames(pd1),colnames(exp1))]
+if(!identical(rownames(pd2),colnames(exp2))) exp2 = exp2[,match(rownames(pd2),colnames(exp2))]
+
+#(3)提取芯片平台编号
+gpl <- eSet2[[1]]@annotation
+
+#(4)合并表达矩阵
 if(F){
   exp1 = limma::normalizeBetweenArrays(exp1)
   exp2 = limma::normalizeBetweenArrays(exp2)
@@ -27,21 +38,8 @@ if(F){
   exp2 = exp2[,-3]
 }
 
-
 exp = cbind(exp1,exp2)
 boxplot(exp)
-
-#exp = log2(exp+1)
-#(2)提取临床信息
-pd1 <- pData(eSet1[[1]])
-pd2 <- pData(eSet2[[1]])
-
-#(3)提取芯片平台编号
-gpl <- eSet2[[1]]@annotation
-
-if(!identical(rownames(pd1),colnames(exp1))) exp1 = exp1[,match(rownames(pd1),colnames(exp1))]
-if(!identical(rownames(pd2),colnames(exp2))) exp2 = exp2[,match(rownames(pd2),colnames(exp2))]
-
 group_list1 = ifelse(str_detect(pd1$title,"Tumour"),"Tumour","Normal")
 group_list2 = ifelse(str_detect(pd2$source_name_ch1,"Paracancerous"),"Normal","Tumour")[-3]
 
